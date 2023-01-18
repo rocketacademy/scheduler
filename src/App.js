e604426acba7b1d71017425a27b8b2a5b9616580
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-01-18 12:27:05 +0800

    removing ptbc4 and adding ftbc10 altered m3-m4

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { HashRouter, Switch, Route } from "react-router-dom";
 import ftbc9 from "./data/17-10-2022_23-02-2023_FTBC9.json";
 import ftbc10 from "./data/30-01-2023_22-05-2023_FTBC10.json";
 
-import ptbc4 from "./data/14-05-2022_07-01-2023_PTBC4.json";
 import ptbc5 from "./data/30-08-2022_29-04-2023_PTBC5.json";
 import ptbc6 from "./data/26-11-2022_29-07-2023_PTBC6.json";
 
@@ -15,7 +14,6 @@ function App() {
   const batchArray = [
     { name: "ftbc9", content: ftbc9 },
     { name: "ftbc10", content: ftbc10 },
-    { name: "ptbc4", content: ptbc4 },
     { name: "ptbc5", content: ptbc5 },
     { name: "ptbc6", content: ptbc6 },
   ];

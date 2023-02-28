bd9bb8523f3bc0b01f0460ee73776c3df8561545
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-02-28 09:43:50 +0800

    adding in Hong kong public holidays FTBC11

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
 import ftbc10 from "./data/FTBC10_2023-01-30_2023-05-18.json"; // FTBC3.1
-import ftbc11 from "./data/FTBC11_2023-03-20_2023-07-06.json"; // FTBC3.1
-import ftbc12 from "./data/FTBC12_2023-05-22_2023-09-07.json"; // FTBC3.1
+import ftbc11 from "./data/FTBC11_2023-03-20_2023-07-06.json"; // FTBC3.1 - Hk public holidays
+import ftbc12 from "./data/FTBC12_2023-05-22_2023-09-07.json"; // FTBC3.1 - Hk public holidays
 
 import ptbc5 from "./data/PTBC5_2022-08-30_2023-04-29.json"; // PTBC3.0, courseStartDaysOfWeekIndex 0 (Tue)
 import ptbc6 from "./data/PTBC6_2022-11-26_2023-07-29.json"; // PTBC3.0, courseStartDaysOfWeekIndex 1 (Sat)
-import ptbc7 from "./data/PTBC7_2023-03-18_2023-10-31.json"; // PTBC3.1
+import ptbc7 from "./data/PTBC7_2023-03-18_2023-11-07.json"; // PTBC3.1, courseStartDaysOfWeekIndex 1 (Sat) - HK public holidays
 // import ptbc8 from "./data/PTBC8_2023-06-17_2024-01-23.json"; // PTBC3.1
 
 import ScheduleContainer from "./components/schedules/ScheduleContainer";

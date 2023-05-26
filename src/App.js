4aca549a2b69f0429e6918dd99ee423c4a56af20
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-05-26 09:38:43 +0800

    removing ftbc10

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
-import ftbc10 from "./data/FTBC10_2023-01-30_2023-05-18.json"; // FTBC3.1
 import ftbc11 from "./data/FTBC11_2023-03-20_2023-07-06.json"; // FTBC3.1
 import ftbc12 from "./data/FTBC12_2023-05-22_2023-09-07.json"; // FTBC3.1 - Hk public holidays
 
-import ptbc5 from "./data/PTBC5_2022-08-30_2023-04-29.json"; // PTBC3.0, courseStartDaysOfWeekIndex 0 (Tue)
 import ptbc6 from "./data/PTBC6_2022-11-26_2023-07-29.json"; // PTBC3.0, courseStartDaysOfWeekIndex 1 (Sat)
 import ptbc7 from "./data/PTBC7_2023-03-18_2023-10-31.json"; // PTBC3.1, courseStartDaysOfWeekIndex 1 (Sat)
 import ptbc8 from "./data/PTBC8_2023-06-24_2024-03-02.json"; // PTBC3.1
@@ -15,11 +13,9 @@ import BatchScheduleGenerator from "./components/BatchScheduleGenerator";
 
 function App() {
   const batchArray = [
-    { name: "ftbc10", content: ftbc10 },
     { name: "ftbc11", content: ftbc11 },
     { name: "ftbc12", content: ftbc12 },
 
-    { name: "ptbc5", content: ptbc5 },
     { name: "ptbc6", content: ptbc6 },
     { name: "ptbc7", content: ptbc7 },
     { name: "ptbc8", content: ptbc8 },

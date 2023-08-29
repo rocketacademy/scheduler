a17f82b58d9e90a560cf6b36bd7902e056c24abc
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-08-29 16:37:15 +0800

    adding ftbc14

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Switch, Route } from "react-router-dom";
 
 import ftbc12 from "./data/FTBC12_2023-05-22_2023-09-07.json"; // FTBC3.1 - Hk public holidays
 import ftbc13 from "./data/FTBC13_2023-08-28_2023-12-14.json"; // FTBC3.1 - Hk public holidays
+import ftbc14 from "./data/FTBC14_2023-11-13_2024-03-18.json";
 
 import ptbc7 from "./data/PTBC7_2023-03-18_2023-10-31.json"; // PTBC3.1, courseStartDaysOfWeekIndex 1 (Sat)
 import ptbc8 from "./data/PTBC8_2023-06-24_2024-02-27.json"; // FTBC3.1 - Hk public holidays
@@ -16,6 +17,7 @@ function App() {
   const batchArray = [
     { name: "ftbc12", content: ftbc12 },
     { name: "ftbc13", content: ftbc13 },
+    { name: "ftbc14", content: ftbc14 },
 
     { name: "ptbc7", content: ptbc7 },
     { name: "ptbc8", content: ptbc8 },

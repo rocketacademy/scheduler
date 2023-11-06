947887e365168ebac861b2d37d0af7698103df23
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-11-06 11:12:55 +0800

    adding in schedule templates ftbc14 ptbc10 removing ptbc7

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
 import ftbc13 from "./data/FTBC13_2023-08-28_2023-12-14.json"; // FTBC3.1 - Hk public holidays
-import ftbc14 from "./data/FTBC14_2023-11-13_2024-03-18.json";
+import ftbc14 from "./data/FTBC14_2023-11-13_2024-03-18.json"; // FTBC3.2 - Hk public holidays
 
-import ptbc7 from "./data/PTBC7_2023-03-18_2023-10-31.json"; // PTBC3.1, courseStartDaysOfWeekIndex 1 (Sat)
 import ptbc8 from "./data/PTBC8_2023-06-24_2024-02-27.json"; // FTBC3.1 - Hk public holidays
-
 import ptbc9 from "./data/PTBC9_2023-09-23_2024-05-28.json"; // FTBC3.1 - Hk public holidays
+import ptbc10 from "./data/PTBC10_2024-01-16_2024-09-03.json"; // FTBC3.2 - Hk public holidays
 
 import ScheduleContainer from "./components/schedules/ScheduleContainer";
 import BatchScheduleGenerator from "./components/BatchScheduleGenerator";
@@ -17,9 +16,9 @@ function App() {
     { name: "ftbc13", content: ftbc13 },
     { name: "ftbc14", content: ftbc14 },
 
-    { name: "ptbc7", content: ptbc7 },
     { name: "ptbc8", content: ptbc8 },
     { name: "ptbc9", content: ptbc9 },
+    { name: "ptbc10", content: ptbc10 },
   ];
 
   return (

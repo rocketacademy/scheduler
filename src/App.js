b9da4d6d1544248a062eb2b673406704f2765848
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-11-13 18:38:45 +0800

    FTBC15

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Switch, Route } from "react-router-dom";
 
 import ftbc13 from "./data/FTBC13_2023-08-28_2023-12-14.json"; // FTBC3.1 - Hk public holidays
 import ftbc14 from "./data/FTBC14_2023-11-13_2024-03-18.json"; // FTBC3.2 - Hk public holidays
+import ftbc15 from "./data/FTBC15_2024-02-26_2024-06-18.json"; // FTBC3.2 - Hk public holidays
 
 import ptbc8 from "./data/PTBC8_2023-06-24_2024-02-27.json"; // FTBC3.1 - Hk public holidays
 import ptbc9 from "./data/PTBC9_2023-09-23_2024-05-28.json"; // FTBC3.1 - Hk public holidays
@@ -15,6 +16,7 @@ function App() {
   const batchArray = [
     { name: "ftbc13", content: ftbc13 },
     { name: "ftbc14", content: ftbc14 },
+    { name: "ftbc15", content: ftbc15 },
 
     { name: "ptbc8", content: ptbc8 },
     { name: "ptbc9", content: ptbc9 },
